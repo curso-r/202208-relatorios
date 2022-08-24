18a25c58151529588c59aff92be4d5f46f913ee2
Julio Trecenti <julio.trecenti@gmail.com>
2022-08-24 09:55:40 -0300

    codigos usados em atendimentos

---
+++ praticas/atendimentos.R
@@ -0,0 +1,32 @@
+
+
+mapa <- geobr::read_municipality()
+mapa_mg <- mapa |>
+  dplyr::filter(abbrev_state == "MG")
+
+base_dados <- abjData::pnud_min |>
+  dplyr::filter(ano == 2010, uf_sigla == "MG") |>
+  dplyr::mutate(muni_id = as.numeric(muni_id))
+
+library(ggplot2)
+mapa_mg |>
+  dplyr::inner_join(
+    base_dados,
+    c("code_muni" = "muni_id")
+  ) |>
+  ggplot() +
+  geom_sf(
+    aes(fill = idhm),
+    colour = "black",
+    size = 0.001
+  ) +
+  scale_fill_viridis_b(
+    option = "A", begin = .1, end = .9
+  ) +
+  theme_void() +
+  theme(
+    legend.position = "bottom"
+  ) +
+  labs(
+    title = "O IDH municipal é mais alto na parte oeste de MG?"
+  )
